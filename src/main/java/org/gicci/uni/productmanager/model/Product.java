c50cf1e8200e54d60920b343dd3c879ed3021fd4
Fernando Gicci Hernandes <fgicci@me.com>
2017-04-26 11:26:45 +0100

    develop
    create model classes and setup maven

---
+++ src/main/java/org/gicci/uni/productmanager/model/Product.java
@@ -0,0 +1,74 @@
+package org.gicci.uni.productmanager.model;
+
+public class Product {
+
+	private String name;
+	private int quantity;
+	private double price;
+	
+	public Product() {
+		this.name = "";
+		this.quantity = 0;
+		this.price = 0.0;
+	}
+	
+	public Product(String name, int quantity, double price) {
+		this.name = name;
+		this.quantity = quantity;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", quantity=" + quantity + ", price=" + price + "]";
+	}
+}
